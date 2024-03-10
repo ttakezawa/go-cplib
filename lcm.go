3a631b15d28fa2bba86f0c4b1470b49feeb51cbf
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2024-03-11 03:23:01 +0900

    Remove unnecessary code and comments in LCM function

--- lcm.go
+++ lcm.go
@@ -1,16 +1,12 @@
 package cplib
 
 func LCM(xs ...int) int {
-	// if len(xs) == 2 {
-	// 	return xs[0] / _GCD2(xs[0], xs[1]) * xs[1]
-	// }
 	l := xs[0]
 	for _, x := range xs[1:] {
 		l = _LCM2(l, x)
 	}
 	return l
 }
-
 func _LCM2(a, b int) int { return a / _GCD2(a, b) * b }
 func _GCD2(a, b int) int {
 	for b != 0 {
